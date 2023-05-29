339977355197901a2542bb7e541c95cdcec0e5a2
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-29 16:34:39 +0200

    feat: create MainProjects component imported from AppMAin

---
+++ src/components/Projects/MainProjects.vue
@@ -0,0 +1,102 @@
+<script>
+// importo lo store
+import {store} from '../../store.js';
+// importo axios
+import axios from 'axios';
+
+// importo i componenti
+import ProjectCard from './ProjectCard.vue';
+
+export default {
+  name: 'AppMain',
+  data(){
+    return{
+      store,
+      currentPage: 1,
+      lastPage: '',
+    }
+  }, 
+  components: {
+    ProjectCard,
+  },    
+  methods: {
+    // funzione che fa la chiamata axios
+    getProjects(){
+      axios.get(this.store.URI+this.store.API + '?page=' + this.currentPage).then(response => {
+        console.log(response.data.results);
+        this.store.projects = response.data.results.data;
+        this.lastPage = response.data.results.last_page;
+      });
+    }, 
+    nextPage(){
+      if(this.currentPage == this.lastPage){
+        this.currentPage = 1;
+      } else {
+        this.currentPage++;
+      }
+      this.getProjects();
+    }, 
+    prevPage(){
+      if(this.currentPage == 1){
+        this.currentPage = this.lastPage;
+      } else {
+        this.currentPage--;
+      }
+      this.getProjects();
+    }
+  }, 
+  created(){
+    this.getProjects();
+  }
+}
+</script>
+
+<template>
+    <div v-if="this.store.projects.length > 0">
+      <h1>All Projects</h1>
+        <div class="cards">
+          <ProjectCard v-for="project in this.store.projects" :project="project"></ProjectCard>
+        </div>
+    </div>
+    <div v-else class="loading-screen">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+    <div class="button-section">
+      <button @click="prevPage()" class="btn btn-secondary">Next Page</button>
+      <span>{{ this.currentPage }} / {{ this.lastPage }}</span>
+      <button @click="nextPage()" class="btn btn-secondary">Next Page</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.cards{
+  display: flex;
+  flex-flow: row wrap;
+  gap: 1.5em;
+  // flex
+  padding: 2em 0;
+}
+.loading-screen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100%;
+  min-height: 600px;
+}
+.button-section{
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: center;
+  gap: 1em;
+  // flex
+  button, a{
+    font-size: 1em;
+  }
+}
+
+
+</style>
