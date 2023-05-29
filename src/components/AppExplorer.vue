b14b92fd3ccdae4dee5c88682178213216a7fc64
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-29 17:00:34 +0200

    feat: handled links in explorer component

--- src/components/AppExplorer.vue
+++ src/components/AppExplorer.vue
@@ -1,8 +1,19 @@
 <script>
+// importo lo store
+import {store} from '../store.js';
+
+
 export default{
   name: 'AppExplorer',
   data(){
-    return {}
+    return {
+      store,
+    }
+  }, 
+  methods: {
+    setActiveLink(index){
+      this.store.activeLink = index;
+    }
   }
 }
 </script>
@@ -17,23 +28,23 @@ export default{
     <div class="files">
       <div class="home flex">
         <img src="/img/html_icon.svg" alt="" class="explorer-img">
-        <a href="#">home.html</a>
+        <a @click="setActiveLink(0)" href="#">home.html</a>
       </div>
       <div class="about flex">
         <img src="/img/css_icon.svg" alt="" class="explorer-img">
-        <a href="#">about.css</a>
+        <a @click="setActiveLink(1)" href="#">about.css</a>
       </div>
       <div class="projects flex">
         <img src="/img/js_icon.svg" alt="" class="explorer-img">
-        <a href="#">projects.js</a>
+        <a @click="setActiveLink(2)" href="#">projects.js</a>
       </div>
       <div class="github flex">
         <img src="/img/markdown_icon.svg" alt="" class="explorer-img">
-        <a href="#">github.md</a>
+        <a @click="setActiveLink(3)" href="#">github.md</a>
       </div>
       <div class="contact flex">
         <img src="/img/json_icon.svg" alt="" class="explorer-img">
-        <a href="#">contact.json</a>
+        <a @click="setActiveLink(4)" href="#">contact.json</a>
       </div>
     </div>
   </div>
