4c5b00351bdad1b980afc21408efabc58f66e3e1
alessandroamormino <amormino.alessandro97@gmail.com>
2023-06-01 22:28:25 +0200

    feat: add fade vue transition between routes

--- src/App.vue
+++ src/App.vue
@@ -28,13 +28,31 @@ export default {
     <AppSide></AppSide>
     <AppExplorer></AppExplorer>
     <main>
-      <router-view></router-view>
+      <router-view v-slot="{Component}">
+        <transition name="fade" mode="out-in">
+          <div :key="$route.path">
+            <component :is="Component" ></component>
+          </div>
+        </transition>
+      </router-view>
     </main>
   </div>
   <AppFooter></AppFooter>
 </template>
 
 <style lang="scss" scoped>
+
+// transition pages
+.fade-enter-active,
+.fade-leave-active{
+  transition: opacity .5s;
+}
+
+.fade-enter,
+.fade-leave-to{
+  opacity: 0;
+}
+
 main{
   background-image: url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='30' height='30' patternTransform='scale(1) rotate(45)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 100%, 0)'/><path d='M3.25 10h13.5M10 3.25v13.5' transform='translate(5,0)' stroke-linecap='square' stroke-width='0.5' stroke='hsla(174, 24%, 30%, 0.4)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>");
   color: #fff;
